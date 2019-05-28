62e9b6b770944a5b8b45eeab663ee99508561277
laucargui <laucargui@gmail.com>
2019-05-28 11:05:08 +0200

    Prueba hospedaje

--- ProyectoVivienda/src/hospedaje/Hospedaje.java
+++ ProyectoVivienda/src/hospedaje/Hospedaje.java
@@ -1,5 +1,31 @@
 package hospedaje;
 
-public class Hospedaje {
+import tipos.TpPAGO;
 
+public class Hospedaje {
+	
+	private TpPAGO TipoPAGO;
+
+	public Hospedaje() {
+	}
+
+	public Hospedaje(TpPAGO tipoPAGO) {
+		this.TipoPAGO = tipoPAGO;
+	}
+
+	public TpPAGO getTipoPAGO() {
+		return TipoPAGO;
+	}
+
+	public void setTipoPAGO(TpPAGO tipoPAGO) {
+		TipoPAGO = tipoPAGO;
+	}
+	@Override
+	public String toString() {
+		return "Hospedaje [TipoPAGO=" + TipoPAGO + ", getTipoPAGO()=" + getTipoPAGO() + "]";
+
+	}
+	
 }
+
+
