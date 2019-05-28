10095cda28905940172e4d83a34127debf9fd176
Juan <juantibc@hotmail.com>
2019-05-28 11:36:09 +0200

    espacio

--- ProyectoVivienda/src/utilidades/Teclado.java
+++ ProyectoVivienda/src/utilidades/Teclado.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 public class Teclado {
 
 	public static String pideDatoCadena(String pregunta) {
+		
 		@SuppressWarnings("unused")
 		BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
 		String respuesta = "";
