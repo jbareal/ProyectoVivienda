35599d8c6d4da451d3dbdf1ee975b642078ab3f4
Juan <juantibc@hotmail.com>
2019-05-28 11:25:42 +0200

    Version 1.0 clase Particular

---
+++ ProyectoVivienda/src/unifamiliar/Particular.java
@@ -0,0 +1,56 @@
+package unifamiliar;
+
+/**
+ * 
+ */
+
+
+import tipos.TpCAS;
+import unifamiliar.Unifamiliar;
+
+/**
+ * @author JUAN
+ *
+ */
+public class Particular extends Unifamiliar {
+	private Integer numBaños; 
+    private TpCAS tipoCasa; 
+    
+		public Particular() {
+		super();
+	}
+
+		/**
+		 * @param numCatastro
+		 * @param superficie
+		 * @param numAireAcond
+		 */
+		public Particular(String numCatastro, Integer superficie, Integer numAireAcond, Integer numBaños, TpCAS tipoCasa) {
+			super(numCatastro, superficie, numAireAcond);
+			
+		}
+
+		public Integer getNumBaños() {
+			return numBaños;
+		}
+
+		public void setNumBaños(Integer numBaños) {
+			this.numBaños = numBaños;
+		}
+
+		public TpCAS getTipoCasa() {
+			return tipoCasa;
+		}
+
+		public void setTipoCasa(TpCAS tipoCasa) {
+			this.tipoCasa = tipoCasa;
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder text = new StringBuilder(super.toString());
+			return " Particular [" + text +" , +Particular [getNumBaños()=" + getNumBaños() + ", getTipoCasa()=" + getTipoCasa() + "]";
+		}
+
+	
+}
