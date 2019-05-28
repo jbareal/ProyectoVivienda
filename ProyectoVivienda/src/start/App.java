6928afc3ad30c5cc0fea85b4b0d33ae16f260b18
laucargui <laucargui@gmail.com>
2019-05-28 11:28:54 +0200

    Paquete start, con clase App a modificar. Teclado con dos funiones mas de lectura de teclado.

---
+++ ProyectoVivienda/src/start/App.java
@@ -0,0 +1,41 @@
+package start;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		/*int opcion = 0;
+		do {
+			mostrarMenu();
+			opcion = Teclado.pideDatoEntero("Opción(0-Salir):");
+			ejecutarOpcion(opcion);
+		} while (opcion != 0);
+
+	}
+
+	private static void ejecutarOpcion(int opcion) {
+		switch (opcion) {
+		case 1:
+			Estructurados.intercambio();
+			break;
+		case 2:Estructurados.potencia();break;
+		case 3: Estructurados.dado(); break;
+		case 4: Estructurados.quiniela(); break;
+		}
+	}
+	
+	CAMBIAR LOS NOMBRES DE LOS CASOS
+	
+
+	private static void mostrarMenu() {
+		System.out.println("\nMenu principal");
+		System.out.println("1. Añadir nueva vivienda.");
+		System.out.println("2. Modificar una vivienda.");
+		System.out.println("3.Borrar una vivienda.");
+		System.out.println("4.Listar el contenido de viviendas.");
+	}
+*/
+
+	}
+
+}
--- ProyectoVivienda/src/utilidades/Teclado.java
+++ ProyectoVivienda/src/utilidades/Teclado.java
@@ -1,16 +1,42 @@
 package utilidades;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Teclado {
 
 	public static String pideDatoCadena(String pregunta) {
+		@SuppressWarnings("unused")
 		BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
 		String respuesta = "";
 		System.out.print(pregunta);
 		return respuesta;
-		
 	}
+	
+	public static Integer pideDatoEntero(String pregunta) {
+		BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+		String respuesta ="";
+		System.out.print(pregunta);
+		try {
+			respuesta = teclado.readLine();
+		}catch (IOException e) {
+		}
+		 
+		return Integer.parseInt(respuesta);
+	}
+
+	public static Double pideDatoDecimal(String pregunta) {
+		BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+		String respuesta ="";
+		System.out.print(pregunta);
+		try {
+			respuesta = teclado.readLine();
+		}catch (IOException e) {
+		}
+		 
+		return Double.parseDouble(respuesta);
+	}
+
 
 }
