1b60177a95187587c684bf7855a300dc10042d43
Juan <juantibc@hotmail.com>
2019-05-28 11:22:43 +0200

    version 1.0 clase Unifamiliar

---
+++ ProyectoVivienda/src/unifamiliar/Unifamiliar.java
@@ -0,0 +1,62 @@
+package unifamiliar;
+
+/**
+ * 
+ */
+
+
+import vivienda.Vivienda;
+
+/**
+ * @author JUAN
+ *
+ */
+public class Unifamiliar extends Vivienda {
+	private Integer superficie;
+	private Integer numAireAcond; 
+
+	/**
+	 * 
+	 */
+	public Unifamiliar() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param numCatastro
+	 */
+	public Unifamiliar(String numCatastro, Integer superficie, Integer numAireAcond) {
+		super(numCatastro);
+		setSuperficie(superficie);
+		setNumAireAcond(numAireAcond);
+	}
+
+	public Integer getSuperficie() {
+		return superficie;
+	}
+
+	public void setSuperficie(Integer superficie) {
+		this.superficie = superficie;
+	}
+
+	public Integer getNumAireAcond() {
+		return numAireAcond;
+	}
+
+	public void setNumAireAcond(Integer numAireAcond) {
+		this.numAireAcond = numAireAcond;
+	} 
+
+	
+	@Override
+	public String toString() {
+		StringBuilder text = new StringBuilder(super.toString());
+		return "Unifamiliar [" + text + ", superficie=" + getSuperficie() + ", numAireAcond=" + getNumAireAcond() +   "]";
+	}
+
+
+	
+
+
+}
