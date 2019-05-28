72be0fbe5fea7d3306c95d0043c8efe38dd7d409
laucargui <laucargui@gmail.com>
2019-05-28 12:27:44 +0200

    Creada clase hotel con sus constructores y métodos

---
+++ ProyectoVivienda/src/hospedaje/Hotel.java
@@ -0,0 +1,49 @@
+package hospedaje;
+
+import tipos.TpPAGO;
+
+public class Hotel extends Hospedaje {
+	
+	private Integer numHabitaciones;
+	private Integer numComedores;
+
+	public Hotel() {
+	}
+
+	public Hotel(String numCatastro, TpPAGO tipoPAGO,Integer numHabitaciones, Integer numComedores) {
+		super();
+		this.numHabitaciones = numHabitaciones;
+		this.numComedores = numComedores;
+		}
+
+	public Integer getNumHabitaciones() {
+		return numHabitaciones;
+	}
+
+	public void setNumHabitaciones(Integer numHabitaciones) {
+		this.numHabitaciones = numHabitaciones;
+	}
+
+	public Integer getNumComedores() {
+		return numComedores;
+	}
+
+	public void setNumComedores(Integer numComedores) {
+		this.numComedores = numComedores;
+	}
+
+	@Override
+	public Integer getPrecioMedio() {
+		return numHabitaciones+numComedores;
+	}
+
+	@Override
+	public String toString() {
+		return "Hotel [numHabitaciones=" + numHabitaciones + ", numComedores=" + numComedores
+				+ ", getNumHabitaciones()=" + getNumHabitaciones() + ", getNumComedores()=" + getNumComedores()
+				+ ", getPrecioMedio()=" + getPrecioMedio() + "]";
+	}
+	
+	
+
+}
