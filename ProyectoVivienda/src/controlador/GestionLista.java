f801b43083c23a8a7599b536f0e808e88ee33f41
jbareal <37106271+jbareal@users.noreply.github.com>
2019-06-02 17:18:40 +0200

    Update GestionLista.java
    He realizado cambios en la clase GestionListados para poder hacer funcionar el método de modificar en la clase OpcionesMenu

--- ProyectoVivienda/src/controlador/GestionLista.java
+++ ProyectoVivienda/src/controlador/GestionLista.java
@@ -7,24 +7,105 @@ import java.util.List;
 import vivienda.Vivienda;
 
 public class GestionLista {
-
+	
+	public class GestionLista {
+	
+	
 	private static List<Vivienda> lstViviendas = new ArrayList<>();
+
 	
 	
-	public static void anadir(Vivienda v) {
-		lstViviendas.add(v);
+	public static void anadirVac1(Vacacional v) {
+		lstViviendas.add((Vacacional)v);
+	}
+	
+	public static void anadirPension(Pension v) {
+		lstViviendas.add((Pension)v);
 	}
 
-	public static void borrar(int pos) {
-		lstViviendas.remove(pos);
+	public static void anadirHotel(Hotel v) {
+		lstViviendas.add((Hotel)v);
 	}
 
+	public static void anadirParticular(Particular v) {
+		lstViviendas.add((Particular)v);
+	}
+
+	
 	public static Integer tamanio() {
 		return lstViviendas.size();
 	}
 	
-	public static List<Vivienda> getLista(){
-		return lstViviendas;
+	
+	
+   public static void borrar(int pos) {
+	   lstViviendas.remove(pos);
+	      
+   }	
+	
+  public static List<Vivienda> getLista(){
+	  return lstViviendas; 
+  }
+
+  /*
+   public static List<Vivienda>getViviendasPorTipo(String tipo){
+		Stream<Vivienda> v = null;
+		List<Vivienda> list = GestionLista.getLista();
+		Collections.sort(list, (i1,i2) -> i1.getNumCatastro().compareTo(i2.getNumCatastro()));		
+		
+		switch(tipo.toUpperCase()){
+			case "HOTELES":
+				v = list.stream().filter(p-> p instanceof Hotel);
+				break;
+		
+			case "PENSIONES":
+				v = list.stream().filter(p-> p instanceof Pension);
+				break;
+				
+			case "PARTICULAR":
+				v = list.stream().filter(p-> p instanceof Particular);
+				break;
+			case "VACACIONAL":
+				v = list.stream().filter(p->p instanceof Vacacional);
+				break;
+			default:
+				v = list.stream();
+		}
+				
+		return	v.collect(Collectors.toList());	
 	}
 
-}
+*/
+	
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+
